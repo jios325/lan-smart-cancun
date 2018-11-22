fff16a06c5ed92df0e0e8396319e73385919e15c
ArelyCoca25 <acontreras@oasishoteles.com>
2018-11-22 11:22:55 -0500

    estilos sin no script pal firefox

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -2,12 +2,5 @@
 <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="preload" as="style" rel="stylesheet" href='https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/css/libraries.min.css' onload="this.onload=null;this.rel='stylesheet'">
-<noscript><link rel="stylesheet" href='https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/css/libraries.min.css'></noscript>
-@if(App::environment('local'))
-    <link rel="preload" as="style" rel="stylesheet" href="{{mix('css/estilos.css')}}" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript><link rel="stylesheet" href="{{mix('css/estilos.css')}}"></noscript>
-@else
-    <link rel="preload" as="style" rel="stylesheet" href="{{mix('css/estilos.css')}}" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript><link rel="stylesheet" href="{{mix('css/estilos.css')}}"></noscript>
-@endif
+<link rel="stylesheet" href='https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/css/libraries.min.css'>
+<link rel="stylesheet" href="{{mix('css/estilos.css')}}"></noscript>
